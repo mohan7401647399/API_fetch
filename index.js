6a6faa49e099e43a70b4ed1f4294c4955e6ea46d
mohan7401647399 <mtm.kcs@gmail.com>
2023-10-09 07:17:43 +0530

    final

--- index.js
+++ index.js
@@ -13,24 +13,23 @@ document.body.append(container)
 
 function bar(value) {
     for (var i = 0; i < value.length; i++) {
-        console.log(value[i]);
         var name = value[i].name;
-        var flag = value[i].flag;
+        var flag = value[i].flags.png;
         var capital = value[i].capital;
         var region = value[i].region;
         var code = value[i].alpha3Code;
 
         row.innerHTML += `
-        <div class="col-md-4 p-3 h-100 w-100 m-auto">
-            <div class="card border-primary mb-3" style="max-width: 18rem; text-align:center">
+        <div class="col-md-3 p-3">
+            <div class="card h-100 border-primary mb-3" style="max-width: 18rem; text-align:center">
             <div class=" card-header p-3 mb-2 bg-dark text-white">${name}</div>
-                <img style="width:100% height:100%" src="${flag}" alt="">
+                <img src="${flag}" alt="flags">
                     <div class="card-body text-primary">
-                    <p class="card-text">Country: ${capital}</p>
-                    <p class="card-text">Region: ${region}</p>
-                    <p class="card-text">Country code: ${code}</p>
-                    <button type="button" class="btn btn-secondary">Click for weather</button>
-                </div>
+                        <p class="card-text">Country: ${capital}</p>
+                        <p class="card-text">Region: ${region}</p>
+                        <p class="card-text">Country code: ${code}</p>
+                    </div>
+                    <button type="button" m-auto class="btn btn-secondary">Click for weather</button>
             </div>
         </div>
         `
